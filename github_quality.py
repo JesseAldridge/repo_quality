38f77d85078d1b9bcfd6713965251e6faacf6419
Jesse Aldridge <JesseAldridge@gmail.com>
2015-11-22 15:26:30 -0800

    hardcode issue counts for projects not using github issues

--- github_quality.py
+++ github_quality.py
@@ -1,6 +1,17 @@
-import json, os
+import json, os, re
 
 import requests, arrow
+
+def pull_most_starred():
+  resp = requests.get(
+    'https://api.github.com/search/repositories?q=stars:>1&s=stars&order=desc')
+
+  print json.dumps(json.loads(resp.content), indent=2)
+
+  print 'rate-limit remaining:', resp.headers['x-ratelimit-remaining']
+  next_link = re.search('<(.+?)>', resp.headers['link']).group(1)
+  print 'next_link:', next_link
+
 
 if not os.path.exists('cache'):
   os.mkdir('cache')
@@ -9,13 +20,14 @@ repo_dicts = []
 for path in [
   # libs that have worked well
   'twbs/bootstrap', 'kennethreitz/requests', 'jasmine/jasmine', 'rails/rails',
-  'angular/angular.js', 'tax/python-requests-aws',
+  'angular/angular.js', 'tax/python-requests-aws', 'django/django', 'mitsuhiko/flask',
+  'npm/npm', 'asweigart/pyperclip',
 
   # libs I haven't tried
-  'Microsoft/TypeScript', 'meteor/meteor', 'facebook/react',  'angular/angular',
+  'Microsoft/TypeScript', 'meteor/meteor', 'facebook/react', 'angular/angular',
 
   # libs that have given me trouble
-  'sindresorhus/atom-jshint', 'npm/npm', 'angular-ui-tree/angular-ui-tree',
+  'sindresorhus/atom-jshint', 'angular-ui-tree/angular-ui-tree',
   'boto/boto', 'rupa/z', 'lsegal/atom-runner']:
   cache_path = os.path.join('cache', path.replace('/', '_') + '.txt')
   if not os.path.exists(cache_path):
@@ -31,14 +43,21 @@ for path in [
   repo_dict['age'] = arrow.now() - arrow.get(repo_dict['created_at'])
   repo_dict['score'] = (
     repo_dict['stargazers_count'] * .01 +
-    repo_dict['stargazers_count'] / repo_dict['open_issues_count'] * 20 +
     repo_dict['stargazers_count'] / repo_dict['age'].days * 2)
+
+  # (need to hardcode issue counts for projects which don't use github for issues)
+  issue_count = (
+    repo_dict['open_issues_count'] if repo_dict['has_issues'] else
+    {'django/django':1209}[path])
+  repo_dict['score'] += repo_dict['stargazers_count'] / issue_count * 20
   repo_dicts.append(repo_dict)
 
 for repo_dict in sorted(repo_dicts, key=lambda d: -d['score']):
   print '       path:', repo_dict['path']
   print '      stars:', repo_dict['stargazers_count']
   print '     issues:', repo_dict['open_issues_count']
+  print ' has_issues:', repo_dict['has_issues']
+
   print '        age:', repo_dict['age']
   print 'star/issues:', repo_dict['stargazers_count'] / repo_dict['open_issues_count']
   print '   star/age:', repo_dict['stargazers_count'] / repo_dict['age'].days
