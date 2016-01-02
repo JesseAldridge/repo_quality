42c1a0f2eb5d7f898cfa8031418410a5192cc6db
Jesse Aldridge <JesseAldridge@gmail.com>
2016-01-02 12:59:09 -0800

    turn into web app

--- github_quality.py
+++ github_quality.py
@@ -3,9 +3,13 @@ import json, os, re, getpass
 import requests, arrow
 from requests import auth
 
-cache_dir_path = os.path.expanduser('~/.github_quality')
-if not os.path.exists(cache_dir_path):
-  os.mkdir(cache_dir_path)
+import secrets, config
+
+if not os.path.exists(config.cache_dir_path):
+  os.mkdir(config.cache_dir_path)
+
+class g:
+  mean_stars_per_issue = 10
 
 def pull_most_starred():
   resp = requests.get(
@@ -18,36 +22,51 @@ def pull_most_starred():
   print 'next_link:', next_link
 
 def pull_all():
-  use_auth = False
-  auth_ = auth.HTTPBasicAuth('JesseAldridge', getpass.getpass()) if use_auth else None
+  use_auth = True
+  auth_ = auth.HTTPBasicAuth('JesseAldridge', secrets.api_key) if use_auth else None
   repo_dicts = []
+  min_score, max_score = None, None
+  stars_per_issue_list = []
   for path in [
     # libs that have worked well
-    # 'twbs/bootstrap', 'kennethreitz/requests', 'jasmine/jasmine', 'rails/rails',
-    # 'angular/angular.js', 'tax/python-requests-aws', 'django/django', 'mitsuhiko/flask',
-    # 'npm/npm', 'asweigart/pyperclip', 'JesseAldridge/github_quality', 'fabric/fabric',
+    'twbs/bootstrap', 'kennethreitz/requests', 'jasmine/jasmine', 'rails/rails',
+    'angular/angular.js', 'tax/python-requests-aws', 'django/django', 'mitsuhiko/flask',
+    'npm/npm', 'asweigart/pyperclip', 'JesseAldridge/github_quality', 'fabric/fabric',
 
     # python unit testing libs
     'nose-devs/nose', 'gabrielfalcao/lettuce', 'pytest-dev/pytest', 'nose-devs/nose2',
-    'docopt/docopt', 'gabrielfalcao/sure', 'rlisagor/freshen',
+    'docopt/docopt', 'gabrielfalcao/sure', 'rlisagor/freshen', 'behave/behave',
 
     # libs I haven't tried much
-    # 'Microsoft/TypeScript', 'meteor/meteor', 'facebook/react', 'angular/angular',
-    # 'strongloop/express', 'dscape/nano', 'Level/levelup', 'felixge/node-mysql',
-    # 'mongodb/node-mongodb-native', 'brianc/node-postgres', 'NodeRedis/node_redis',
-    # 'mapbox/node-sqlite3', 'mafintosh/mongojs', 'tornadoweb/tornado', 'gevent/gevent',
-    # 'percolatestudio/meteor-migrations', 'Polymer/polymer', 'Automattic/mongoose',
-    # 'nodejs/node', 'sequelize/sequelize', 'Automattic/monk', 'balderdashy/waterline',
-    # 'balderdashy/sails', 'playframework/playframework', 'pyinvoke/invoke',
+    'Microsoft/TypeScript', 'meteor/meteor', 'facebook/react', 'angular/angular',
+    'strongloop/express', 'dscape/nano', 'Level/levelup', 'felixge/node-mysql',
+    'mongodb/node-mongodb-native', 'brianc/node-postgres', 'NodeRedis/node_redis',
+    'mapbox/node-sqlite3', 'mafintosh/mongojs', 'tornadoweb/tornado', 'gevent/gevent',
+    'percolatestudio/meteor-migrations', 'Polymer/polymer', 'Automattic/mongoose',
+    'nodejs/node', 'sequelize/sequelize', 'Automattic/monk', 'balderdashy/waterline',
+    'balderdashy/sails', 'playframework/playframework', 'pyinvoke/invoke',
+    'msanders/snipmate.vim',
 
     # libs that have given me trouble
-    # 'sindresorhus/atom-jshint', 'angular-ui-tree/angular-ui-tree',
-    # 'boto/boto', 'rupa/z', 'lsegal/atom-runner'
+    'sindresorhus/atom-jshint', 'angular-ui-tree/angular-ui-tree',
+    'boto/boto', 'rupa/z', 'lsegal/atom-runner'
     ]:
     repo_dict = pull_repo(path, auth=auth_)
+    if min_score is None or repo_dict['score'] < min_score:
+      min_score = repo_dict['score']
+    if max_score is None or repo_dict['score'] > max_score:
+      max_score = repo_dict['score']
+    stars_per_issue_list.append(float(repo_dict['stargazers_count'] / repo_dict['issue_count']))
     if not repo_dict:
       print 'null repo:', path, repo_dict
     repo_dicts.append(repo_dict)
+  g.mean_stars_per_issue = (
+    sum(stars_per_issue_list) / float(len(stars_per_issue_list))
+    if stars_per_issue_list else g.mean_stars_per_issue)
+
+  json_out = json.dumps({'min_score': min_score, 'max_score': max_score}, indent=2)
+  with open(os.path.join(config.cache_dir_path, 'min_max.json'), 'w') as f:
+    f.write(json_out)
 
   for repo_dict in sorted(repo_dicts, key=lambda d: -d['score']):
     print '        path:', repo_dict['path']
@@ -69,12 +88,19 @@ def pull_all():
     print ' ', repo_dict['score']
 
 
+hardcoded_issue_counts = {
+  'django/django': 1209,
+  # (node-mongodb's jira page only lists 14 issues; but you have to make an account to create an
+  #  issue, so I'm assuming that if they used GitHub issues they would have 10x more.)
+  'mongodb/node-mongodb-native': 140
+}
+
 def pull_repo(path, auth=None):
-  if not re.match('[A-Za-z0-9_\-]+', path):
+  if not re.match('[A-Za-z0-9_/-]+', path):
     return 'illegal char in path'
   if len(path) > 100:
     return 'path too long'
-  cache_file_path = os.path.join(cache_dir_path, path.replace('/', '_') + '.txt')
+  cache_file_path = os.path.join(config.cache_dir_path, path.replace('/', '_') + '.txt')
   if not os.path.exists(cache_file_path):
     print 'pulling info:', cache_file_path
     resp = requests.get('https://api.github.com/repos/' + path, auth=auth)
@@ -93,12 +119,14 @@ def pull_repo(path, auth=None):
     repo_dict['stargazers_count'] / repo_dict['age'].days * 2)
 
   # (need to hardcode issue counts for projects which don't use github for issues)
-  issue_count = (
-    repo_dict['open_issues_count'] if repo_dict['has_issues'] else
-    {'django/django':1209,
-    # (node-mongodb's jira page only lists 14 issues; but you have to make an account to create an
-    #  issue, so I'm assuming that if they used GitHub issues they would have 10x more.)
-    'mongodb/node-mongodb-native':140}[path])
+  print 'mean_stars_per_issue:', g.mean_stars_per_issue
+  if repo_dict['has_issues']:
+    issue_count = repo_dict['open_issues_count']
+  elif path in hardcoded_issue_counts:
+    issue_count = hardcoded_issue_counts[path]
+  else:
+    issue_count = repo_dict['stargazers_count'] / g.mean_stars_per_issue
+  repo_dict['issue_count'] = issue_count
   repo_dict['score'] += repo_dict['stargazers_count'] / (issue_count or 1) * 20
   return repo_dict
 
--- serve.py
+++ serve.py
@@ -3,16 +3,21 @@ import os, json, datetime
 import flask
 from flask import request
 
-import github_quality
+import github_quality, config
 
 app = flask.Flask(__name__)
 
+app.jinja_env.variable_start_string='((('
+app.jinja_env.variable_end_string=')))'
+app.comment_start_string='((#'
+app.comment_end_string='#))'
+
 @app.route('/')
 def index():
-    print 'hello'
-    res = flask.render_template('index.html')
-    print 'rendered:', res
-    return res
+  print 'hello'
+  res = flask.render_template('index.html')
+  print 'rendered:', res
+  return res
 
 class DateTimeEncoder(json.JSONEncoder):
   def default(self, obj):
@@ -25,16 +30,22 @@ class DateTimeEncoder(json.JSONEncoder):
     else:
       return super(DateTimeEncoder, self).default(obj)
 
-@app.route('/check')
-def render():
-    repo = request.args.get('repo')
-    res = github_quality.pull_repo(repo)
+@app.route('/<username>/<repo_name>')
+def query_repo(username, repo_name):
+  print 'request.args:', request.args
+  repo = '/'.join((username, repo_name))
+  repo_dict = github_quality.pull_repo(repo)
 
-    print 'res:', res, type(res)
-    json_out = DateTimeEncoder().encode(res)
-    return flask.render_template('main.html', json_out=json_out)
+  with open(os.path.join(config.cache_dir_path, 'min_max.json')) as f:
+    min_max = json.loads(f.read())
+  # json_out = DateTimeEncoder().encode(repo_dict)
+  filtered_dict = {k: repo_dict[k] for k in ('full_name', 'score', 'has_issues')}
+  repo_json = json.dumps(filtered_dict)
+  return flask.render_template(
+    'main.html', repo_json=repo_json,
+    min_score=min_max['min_score'], max_score=min_max['max_score'])
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=port, debug=port==3000)
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 3000))
+  app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', True))
