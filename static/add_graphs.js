f653bc5bcd98df0223857b690218397836a08164
Jesse Aldridge <jessealdridge@gmail.com>
2018-07-12 00:53:51 -0700

    add debug log

--- static/add_graphs.js
+++ static/add_graphs.js
@@ -3,6 +3,9 @@ function add_graphs(repo_list) {
     var data_objs = [];
 
     var iso_dt_to_score = repo_obj.timestamp_to_score;
+
+    console.log('adding graph:', iso_dt_to_score);
+
     Object.keys(iso_dt_to_score).forEach(function(iso_dt) {
       data_objs.push({
         timestamp: new Date(Date.parse(iso_dt)),
