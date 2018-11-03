6c5163ff979983f27ba832f372cbdc070ecca406
Jesse Aldridge <jessealdridge@gmail.com>
2018-11-03 01:45:39 -0700

    excel parsing

--- _0_repo_lists.py
+++ _0_repo_lists.py
@@ -583,6 +583,17 @@ repo_lists = collections.OrderedDict([
       'caffe2/caffe2',
       'davisking/dlib',
   ]),
+  ('excel_parsing', [
+    # https://bitbucket.org/openpyxl/openpyxl
+    'jmcnamara/XlsxWriter',
+    'python-excel/xlrd',
+    'python-excel/xlwt',
+    'python-excel/xlutils',
+    'kz26/PyExcelerate',
+    'sheetjs/js-xlsx',
+    'tealeg/xlsx',
+    '360EntSecGroup-Skylar/excelize',
+  ]),
   ('extract_transform_load_(etl)', [
     'adaptivegenome/openge',
     'airbnb/airflow',
