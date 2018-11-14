98b48f0f6b861775988364183518db2c3a13bf60
Jesse Aldridge <jessealdridge@gmail.com>
2018-11-14 01:05:41 -0800

    stuff -> junk

--- stuff/firebase_token.py
+++
@@ -1,7 +0,0 @@
-from firebase_token_generator import create_token
-
-import secrets
-
-auth_payload = { "uid": "admin" }
-token = create_token(secrets.firebase_secret, auth_payload)
-print 'token:', token
