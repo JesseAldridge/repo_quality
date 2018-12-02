45af359a0f8b3222ddfc6052123d1c873d669dfe
Jesse Aldridge <jessealdridge@gmail.com>
2018-12-02 17:15:18 -0600

    add api-test.py

---
+++ api_test.py
@@ -0,0 +1,7 @@
+import _0_hit_api
+
+# resp = _0_hit_api.hit_api('hapijs/hapi', None, '/issues')
+# resp = _0_hit_api.hit_api('hapijs/hapi', None)
+resp = _0_hit_api.hit_api('hapijs/hapi', None, '/commits')
+
+print resp
