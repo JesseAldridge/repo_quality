17f6ba3922a6126e6d55ae3a6a69bb4ab37db748
Jesse Aldridge <jessealdridge@gmail.com>
2018-12-03 10:13:31 -0600

    timestamps

--- _4_separate_loop.py
+++ _4_separate_loop.py
@@ -1,8 +1,11 @@
 import subprocess, time
+from datetime import datetime
 
 debug = 0
 while True:
+  print 'launching subprocess at {}'.format(datetime.now())
   subprocess.Popen(['python', '_3_update_cache_loop.py', str(debug)]).communicate()
+  print 'completed subprocess at {}'.format(datetime.now())
 
   if debug:
     time.sleep(1)
